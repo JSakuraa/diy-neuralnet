4e015bfa36695b6309d4e2ddc34ea565b37f9338
Justin Nappi <jtn8001@gmail.com>
2024-05-26 01:12:32 -0400

    Added pokemon data

--- cleanData.py
+++ cleanData.py
@@ -1,18 +1,48 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('pokemon.csv')
 
-df['total'] = df['hp'] + df['attack'] + df['defense'] + df['sp_attack'] + df['sp_defense'] + df['speed']
+class Pokemon:
 
-new_df = df[['name', 'total', 'capture_rate', 'is_legendary']].copy()
+    def __init__(self, df):
+        data = pd.read_csv(df)
+        data["total"] = (
+            data["hp"]
+            + data["attack"]
+            + data["defense"]
+            + data["sp_attack"]
+            + data["sp_defense"]
+            + data["speed"]
+        )
+        data["total"] = data["total"] - data["total"].mean()
+        data["capture_rate"] = data["capture_rate"] - data["capture_rate"].mean()
+        self.df = data[["name", "total", "capture_rate", "is_legendary"]].copy()
 
-# 
-new_df['total_new'] = df['total'] - df['total'].mean()
+    def returnData(self, nameList):
+        names = self.df[self.df["name"].isin(nameList)].to_numpy()
+        return names[:, 0], names[:, 1:3], names[:, 3]
 
-new_df['capture_rate_new'] = df['capture_rate'] - df['capture_rate'].mean()
+    def getTests(self, testNames):
+        tests = self.df[self.df["name"].isin(testNames)].to_numpy()
+        return tests[:, [0, 3]], tests[:, 1:3]
 
-selected_names = ['Weedle', 'Charmander', 'Starly', 'Mewtwo', 'Dialga']
-selected_rows = new_df[new_df['name'].isin(selected_names)]
 
-print(selected_rows)
+def main():
+    df = "pokemon.csv"
+    pkmn = Pokemon(df)
+    names = ["Weedle", "Charmander", "Starly", "Mewtwo", "Dialga"]
+    tests = ["Wurmple", "Palkia"]
+    test_names, test_data = pkmn.getTests(tests)
+    names, x_data, y_data = pkmn.returnData(names)
+    print("--------------------- Names --------------------")
+    print(names)
+    print("--------------------- X_data --------------------")
+    print(x_data)
+    print("--------------------- Y_data --------------------")
+    print(y_data)
+    print("--------------------- Test_data --------------------")
+    print(test_data)
+
+
+if __name__ == "__main__":
+    main()
